d9d52f51a2c50eb449a32eeb41e93d135022fab2
Komsa Attila <63514374+kms77@users.noreply.github.com>
2020-09-09 22:02:12 +0300

    Add files via upload

---
+++ Semester I/Computer Systems Architecture/Laboratory 1/Laboratory1- three_minus_four/Lab01-three_minus_four.asm
@@ -0,0 +1,26 @@
+;Task
+;Write a program in the assembly language that computes: 3-4=-1
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov AL,3             ; AL=3
+        mov BL,4             ; BL=4 
+        sub AL,BL            ; AL=AL-BL=3-4=-1 =>AL=FF (in hexadecimal)
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
